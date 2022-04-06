2f1b35c05e10f2b4d9f7f280796a517887d69e22
Vikash Kumar <vikashkumar@iiitkalyani.ac.in>
2022-04-07 00:31:31 +0530

    Code Clean

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,19 @@ import express from "express";
 import data from "./data.js";
 
 const app = express();
-
 const PORT = 4000;
 
-app.get('/api/products/:id',(req,res)=>{
-    const product = data.products.find(x=>x._id === req.params.id)
-    if(product){
-        res.send(product)
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found!" });
+  }
+});
 
-    } 
-    else{
-        res.status(404).send({message:'Product Not Found!'})
-    }
-})
+app.get("/api/products", (req, res) => {
+  res.send(data.products);
+});
 
-app.get('/api/products',(req,res)=>{
-    res.send(data.products)
-})
-
-app.listen(PORT,()=>console.log('App is running!'));
-
+app.listen(PORT, () => console.log("App is running!"));
